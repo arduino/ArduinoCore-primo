91fdaab1947cc48257bd0cf2e5ea1a7b3cf1b608
chiararuggeri <chiara@rduino.org>
2016-09-14 11:23:03 +0200

    BLEProperties masks added

--- libraries/BLE/GATT/BLECharacteristic.cpp
+++ libraries/BLE/GATT/BLECharacteristic.cpp
@@ -90,28 +90,28 @@ void BLECharacteristic::pushCharacteristicToSD(uint16_t service_handle){
 	
 	//add all related descriptor to softdevice
 	BLEDescriptor *descriptor=descriptorList.getFirstElement();
-		while(descriptor!=0){
-			descriptor->pushDescriptorToSD(char_handl.value_handle);
-			descriptor=descriptor->getNextElement();
-		}
+	while(descriptor!=0){
+		descriptor->pushDescriptorToSD(char_handl.value_handle);
+		descriptor=descriptor->getNextElement();
+	}
 }
 
 void BLECharacteristic::fillCharStructures(uint8_t properties, uint8_t *data, uint16_t dataLength, bool variableLength){
 	memset(&char_md, 0, sizeof(char_md));
 	//set characteristic properties
-	char_md.char_props.broadcast=properties & 0b00000001;
-	char_md.char_props.read=(properties & 0b00000010)>>1;
-	char_md.char_props.write_wo_resp=(properties & 0b00000100)>>2;
-	char_md.char_props.write=(properties & 0b00001000)>>3;
-	char_md.char_props.notify=(properties & 0b00010000)>>4;
-	char_md.char_props.indicate=(properties & 0b00100000)>>5;
-	char_md.char_props.auth_signed_wr=(properties & 0b01000000)>>6;
+	char_md.char_props.broadcast=(properties & BLEPropertyBroadcastMask) ? 1 : 0;
+	char_md.char_props.read=(properties & BLEPropertyReadMask) ? 1 : 0;
+	char_md.char_props.write_wo_resp=(properties & BLEPropertyWriteCommandMask) ? 1 : 0;
+	char_md.char_props.write=(properties & BLEPropertyWriteMask) ? 1 : 0;
+	char_md.char_props.notify=(properties & BLEPropertyNotifyMask) ? 1 : 0;
+	char_md.char_props.indicate=(properties & BLEPropertyIndicateMask) ? 1 : 0;
+	char_md.char_props.auth_signed_wr=(properties & BLEPropertyAuthSighedWriteMask) ? 1 : 0;
 	//TODO: add user descriptor
 	char_md.p_char_user_desc=NULL;
 	//TODO: add format descriptor
 	char_md.p_char_pf=NULL;
 	//if notify or indicate properties are set, set the cccd's structure
-	if((properties & 0b00110000)!=0){
+	if(((properties & BLEPropertyNotifyMask)!=0) || ((properties & BLEPropertyIndicateMask)!=0)){
 		memset(&cccd_md, 0, sizeof(cccd_md));
 		BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
 		BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
@@ -125,14 +125,14 @@ void BLECharacteristic::fillCharStructures(uint8_t properties, uint8_t *data, ui
 	
 	//set attribute metadata structure
 	memset(&attr_md, 0, sizeof(attr_md));
-	if((properties & 0b00000010)>>1)
+	if(properties & BLEPropertyReadMask)
 		BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
-	if((properties & 0b00001000)>>3)
+	if(properties & BLEPropertyWriteMask)
 		BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
 	attr_md.vloc       =  BLE_GATTS_VLOC_STACK;
 	attr_md.rd_auth    = 0;
 	attr_md.vlen       = variableLength;
-	if((properties & 0b00000100)>>2)
+	if(properties & BLEPropertyWriteCommandMask)
 		attr_md.wr_auth    = 1;
 	
 	//set characteristic value structure
