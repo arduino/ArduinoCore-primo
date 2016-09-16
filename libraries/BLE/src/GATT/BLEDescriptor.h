767f6860cf394893019e4d6cb5b26ff8191aa5ea
chiararuggeri <chiara@rduino.org>
2016-09-16 08:54:05 +0200

    BLEDescriptor: added overloaded constructors

--- libraries/BLE/src/GATT/BLEDescriptor.h
+++ libraries/BLE/src/GATT/BLEDescriptor.h
@@ -29,16 +29,18 @@ class BLEDescriptor : public BLEAttribute {
 	public:
 		BLEDescriptor(const char * uuid, uint8_t *data, uint16_t dataLength);
 		BLEDescriptor(uint16_t shortUuid, uint8_t *data, uint16_t dataLength);
+		BLEDescriptor(const char * uuid, char data[]);
+		BLEDescriptor(uint16_t shortUuid, char data[]);
 		BLEDescriptor * getNextElement(void);
 		void setNextElement(BLEDescriptor * element);
 		void pushDescriptorToSD(uint16_t char_handle);
 		
 	private:
-		BLEDescriptor *nextElement= 0;
-		ble_gatts_attr_t attr_desc_value;
-		ble_uuid_t dUuid;
-		ble_gatts_attr_md_t attr_md;
-		uint16_t * desc_handle;
+		BLEDescriptor        *nextElement = 0;
+		ble_gatts_attr_t     attr_desc_value;
+		ble_uuid_t           dUuid;
+		ble_gatts_attr_md_t  attr_md;
+		uint16_t             *desc_handle;
 };
 
 #endif
