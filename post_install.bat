04fcb3136af7be20e9c18b990dd27f167c6457e6
Arturo Rinaldi <arty.net2@gmail.com>
2017-03-08 13:56:33 +0100

    adding mbed device installer utility

--- post_install.bat
+++ post_install.bat
@@ -2,12 +2,12 @@
 set ARGS=/A /SE /SW /SA
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
   drivers\dpinst-amd64.exe %ARGS%
-  drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
+  drivers\x64\mbed_64.exe
 ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
   drivers\dpinst-amd64.exe %ARGS%
-  drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
+  drivers\x64\mbed_64.exe
 ) ELSE (
   drivers\dpinst-x86.exe %ARGS%
-  drivers\dpinst-x86.exe %ARGS% /path %CD%\drivers\x86
+  drivers\x64\mbed_32.exe
 )
 exit /b 0
