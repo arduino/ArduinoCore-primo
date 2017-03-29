b41d7fabe76397040899e14c34f0822195914488
DHJeff <jeff@doghunter.org>
2017-03-29 15:17:47 +0800

    Included Copyright notice to CIR library

--- libraries/CIR/CIR.h
+++ libraries/CIR/CIR.h
@@ -1,3 +1,29 @@
+/*
+****************************************************************************
+* Copyright (c) 2017 Arduino srl. All right reserved.
+*
+* File : CIR.h
+* Date : 2017/03/10
+* Revision : 0.0.1 $
+* Author: jeff[at]arduino[dot]org
+*
+****************************************************************************
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 #ifndef __CIR_H__
 #define __CIR_H__
 
