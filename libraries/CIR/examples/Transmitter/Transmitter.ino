5b85c5d9d3d31b260a228495f06119440279918a
DHJeff <jeff@doghunter.org>
2017-04-03 15:07:43 +0800

    Added headers and Comments in CIR example (#20)
    * Added headers and Comments in CIR example
    
    * Added library.properties to CIR library

--- libraries/CIR/examples/Transmitter/Transmitter.ino
+++ libraries/CIR/examples/Transmitter/Transmitter.ino
@@ -1,31 +1,72 @@
+/*
+  Transmitter.ino
+
+  Written by Jeff Wu (jeff@arduino.org)
+
+  This example for the Arduino Primo board shows how to use
+  CIR library.
+  The IR transmitter allows you to send hex code by infrared LED
+  of the Primo board, the hex code is based on the NEC protocol
+  that transmiited consits of the following, using buttons to send
+  hex code when a button is pressed.
+
+  NEC Protocol:
+  9ms leading pulse burst
+  4.5ms space
+  8-bit address for the receiving device
+  8-bit logical inverse of the address
+  8-bit command
+  8-bit logical inverse of the command
+  Logical 0 : 560us + 560us
+  Logical 1 : 560us + 1690us
+
+  This example code is in the public domain.
+
+*/
+
 #include <Wire.h>
 #include "CIR.h"
 
+// define hex code:
 #define HEX_1 0xC1AA1107
 #define HEX_2 0xC1AA68F0
 
 CIR Primo_CIR;
 
 void setup() {
+  // initialize the LED pin as an output:
   pinMode(13, OUTPUT);
+  // turn LED off:
   digitalWrite(13, LOW);
+  // enable IR ready to transmitter:
   Primo_CIR.enableTransmitter();
+  // initialize the pushbutton pin as an input:
   pinMode(USER1_BUTTON, INPUT);
   pinMode(USER2_BUTTON, INPUT);
+  // turn LED on:
   digitalWrite(13, HIGH);
 }
 
 void loop() {
+  // check if the USER1 BUTTON is pressed:
   if (!digitalRead(USER1_BUTTON))
   {
+    // turn LED on:
     digitalWrite(13, HIGH);
+
+    // sned hex code:
     Primo_CIR.sendEncode(HEX_1);
+    // turn LED off:
     digitalWrite(13, LOW);
   }
+  // check if the USER2 BUTTON is pressed:
   else if (!digitalRead(USER2_BUTTON))
   {
+    // turn LED on:
     digitalWrite(13, HIGH);
+    // sned hex code:
     Primo_CIR.sendEncode(HEX_2);
+    // turn LED off:
     digitalWrite(13, LOW);
   }
 }
