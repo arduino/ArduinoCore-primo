c3300e4f3b51f794c8b64fed7f82c37c1c94bdc6
Arturo Rinaldi <arty.net2@gmail.com>
2017-05-12 11:57:22 +0200

    now only the mBed drivers are installed

--- post_install.bat
+++ post_install.bat
@@ -1,13 +1,13 @@
 @echo off
 set ARGS=/A /SE /SW /SA
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  drivers\dpinst-amd64.exe %ARGS%
-  drivers\x64\mbed_64.exe
+  :: drivers\dpinst-amd64.exe %ARGS%
+	drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
 ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  drivers\dpinst-amd64.exe %ARGS%
-  drivers\x64\mbed_64.exe
+  :: drivers\dpinst-amd64.exe %ARGS%
+	drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
 ) ELSE (
-  drivers\dpinst-x86.exe %ARGS%
-  drivers\x86\mbed_32.exe
+  :: drivers\dpinst-x86.exe %ARGS%
+	drivers\dpinst-x86.exe %ARGS% /path %CD%\drivers\x86
 )
 exit /b 0
