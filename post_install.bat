967904f9f649b31d4ca95ef78e9562fc03ec03dc
Arturo Rinaldi <arty.net2@gmail.com>
2017-05-12 12:32:04 +0200

    leaving just one installation entry in post_install.bat

--- post_install.bat
+++ post_install.bat
@@ -1,13 +1,10 @@
 @echo off
 set ARGS=/A /SE /SW /SA
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  :: drivers\dpinst-amd64.exe %ARGS%
-	drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
+  drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
 ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  :: drivers\dpinst-amd64.exe %ARGS%
-	drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
+  drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
 ) ELSE (
-  :: drivers\dpinst-x86.exe %ARGS%
-	drivers\dpinst-x86.exe %ARGS% /path %CD%\drivers\x86
+  drivers\dpinst-x86.exe %ARGS% /path %CD%\drivers\x86
 )
 exit /b 0
